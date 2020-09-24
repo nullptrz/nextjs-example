d878fd613cc306d954d507805587f2998e1481fa
nullptrz <ahusuncode@gmail.com>
2020-09-24 23:34:42 +0800

    added life post

---
+++ pages/about-me.tsx
@@ -0,0 +1,29 @@
+import Layout from '../components/layout';
+
+export default function AboutMe() {
+  return (
+    <Layout>
+      <section>
+        <h1>Ali Ahsan Saeed</h1>
+        <h5>Web Developer in the Making</h5>
+      </section>
+      <section>
+        <h3>Introduction</h3>
+        <p>
+          I was born and raised in the capital of Maldives, namely Male' City.
+          but the city was getting overcrowded and hence my family decided to
+          move to the solution that the government came up with for the
+          overcrowding of the capital city, a new artificial Island which is now
+          connected by a bridge named as Hulhumale'. I've been living there
+          since then.
+        </p>
+      </section>
+      <section>
+        <h3>Hobbies</h3>
+      </section>
+      <section>
+        <h3>Interests</h3>
+      </section>
+    </Layout>
+  );
+}
---
+++ posts/life.md
@@ -0,0 +1,10 @@
+---
+title: 'Life as we know it'
+date: '2020-09-24'
+---
+
+Well what exactly is life anyway, when you think about it, sometimes it makes sense
+but other times it doesn't. So how do we even talk about life. But somehow we gotta
+survive and we gotta have what we call a 'life'. Because life is precious. Life is
+the most crucial thing in life. Life is water. A water brand in Maldives. Gotcha!!!
+Hahahaha!
